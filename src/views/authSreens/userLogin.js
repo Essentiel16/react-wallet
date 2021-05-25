2e51cd583249105925c9542fa68b0a5a71ff989f
Abigail Folarin <folarinabigail2020@gmail.com>
2021-05-25 17:00:17 +0100

    add react-dom

--- src/views/authSreens/userLogin.js
+++ src/views/authSreens/userLogin.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomeStructure from '../../components/Layout'
 import Login from "../../components/Login"
+import Modal from '../../components/Modal'
 function userLogin() {
   return (
     <div>
       <HomeStructure title="Log In" subtitle="Great to have you back">
       <Login/>
       </HomeStructure>
+      <button>Click me!</button>
     </div>
   )
 }
